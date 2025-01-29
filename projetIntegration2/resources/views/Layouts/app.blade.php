99a56c1c0586f03c9f2f79f7fc379e9fa957a0b4
Loick Michaud <139803354+naijel2103@users.noreply.github.com>
2025-01-29 14:33:09 -0500

    layout

--- projetIntegration2/resources/views/Layouts/app.blade.php
+++ projetIntegration2/resources/views/Layouts/app.blade.php
@@ -16,23 +16,24 @@
 <meta charset="UTF-8">
 </head>
 
-<body>
-<!-- Navigation principale -->
-<header>
-<aside class="w-20 bg-gray-400 text-white h-screen flex flex-col items-center py-4 space-y-4">
-       <a> <div class="w-16 h-16 rounded-full overflow-hidden"><img src="{{ asset('img/workoutMasterLogo.jpg') }}" class="object-cover w-full h-full"></div></a>
-       <a> <div class="w-16 h-16 rounded-full overflow-hidden"><img src="{{ asset('img/workoutMasterLogo.jpg') }}" class="object-cover w-full h-full"></div></a>
-       <a> <div class="w-16 h-16 rounded-full overflow-hidden"><img src="{{ asset('img/workoutMasterLogo.jpg') }}" class="object-cover w-full h-full"></div></a>
-       <a> <div class="w-16 h-16 rounded-full overflow-hidden"><img src="{{ asset('img/workoutMasterLogo.jpg') }}" class="object-cover w-full h-full"></div></a>
-       <a> <div class="w-16 h-16 rounded-full overflow-hidden"><img src="{{ asset('img/workoutMasterLogo.jpg') }}" class="object-cover w-full h-full"></div></a>
-       <a> <div class="w-16 h-16 rounded-full overflow-hidden"><img src="{{ asset('img/workoutMasterLogo.jpg') }}" class="object-cover w-full h-full"></div></a>
-       <a> <div class="w-16 h-16 rounded-full overflow-hidden"><img src="{{ asset('img/workoutMasterLogo.jpg') }}" class="object-cover w-full h-full"></div></a>
-</aside> 
-</header>
-
-@yield("contenu")
-
-
+<body class=" flex h-screen" id="background">
+    <!-- Sidebar -->
+    <aside class="w-16  text-white h-full flex flex-col items-center py-4 space-y-4" id="sidebar">
+        <div class="w-10 h-10 bg-gray-600 rounded-full"></div>
+        <div class="w-10 h-10 bg-gray-500 rounded-full"></div>
+        <div class="w-10 h-10 bg-red-600 rounded-full"></div>
+        <div class="w-10 h-10 bg-yellow-500 rounded-full"></div>
+        <div class="w-10 h-10 bg-blue-700 rounded-full"></div>
+        <div class="w-10 h-10 bg-white border border-gray-700 rounded-full"></div>
+        <div class="w-10 h-10 bg-blue-900 rounded-full"></div>
+    </aside>
+    
+    <!-- Main Content -->
+    <main id="main">
+        <div>
+          @yield('contenu')
+        </div>
+    </main>
 <!-- Mettre le footer -->
  <footer>
 </footer>
