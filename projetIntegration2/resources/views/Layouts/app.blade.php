d681d87a6e187d9eb39640e4dc51c05d9ab960d9
Loick Michaud <139803354+naijel2103@users.noreply.github.com>
2025-01-29 14:16:11 -0500

    .

--- projetIntegration2/resources/views/Layouts/app.blade.php
+++ projetIntegration2/resources/views/Layouts/app.blade.php
@@ -20,13 +20,13 @@
 <!-- Navigation principale -->
 <header>
 <aside class="w-16 bg-gray-900 text-white h-screen flex flex-col items-center py-4 space-y-4">
-        <div class="w-10 h-10 bg-gray-600 rounded-full"></div>
-        <div class="w-10 h-10 bg-gray-500 rounded-full"></div>
-        <div class="w-10 h-10 bg-red-600 rounded-full"></div>
-        <div class="w-10 h-10 bg-yellow-500 rounded-full"></div>
-        <div class="w-10 h-10 bg-blue-700 rounded-full"></div>
-        <div class="w-10 h-10 bg-white border border-gray-700 rounded-full"></div>
-        <div class="w-10 h-10 bg-blue-900 rounded-full"></div>
+       <a> <div class="w-10 h-10 bg-gray-600 rounded-full"></div>
+       <a> <div class="w-10 h-10 bg-gray-500 rounded-full"></div>
+       <a> <div class="w-10 h-10 bg-red-600 rounded-full"></div>
+       <a> <div class="w-10 h-10 bg-yellow-500 rounded-full"></div>
+       <a><div class="w-10 h-10 bg-blue-700 rounded-full"></div>
+       <a><div class="w-10 h-10 bg-white border border-gray-700 rounded-full"></div>
+       <a> <div class="w-10 h-10 bg-blue-900 rounded-full"></div>
     </aside> 
 </header>
 
