420ff2945aea028b4c9ccc1a602433bf1b8871ec
Loick Michaud <139803354+naijel2103@users.noreply.github.com>
2025-01-29 14:11:46 -0500

    Update app.blade.php

--- projetIntegration2/resources/views/Layouts/app.blade.php
+++ projetIntegration2/resources/views/Layouts/app.blade.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" style="text/css" href="\css\GabaritCss.css">
+        <script src="https://cdn.tailwindcss.com"></script>
         @yield("style")
    
 <meta charset="UTF-8">
@@ -18,7 +19,15 @@
 <body>
 <!-- Navigation principale -->
 <header>
-
+<aside class="w-16 bg-gray-900 text-white h-screen flex flex-col items-center py-4 space-y-4">
+        <div class="w-10 h-10 bg-gray-600 rounded-full"></div>
+        <div class="w-10 h-10 bg-gray-500 rounded-full"></div>
+        <div class="w-10 h-10 bg-red-600 rounded-full"></div>
+        <div class="w-10 h-10 bg-yellow-500 rounded-full"></div>
+        <div class="w-10 h-10 bg-blue-700 rounded-full"></div>
+        <div class="w-10 h-10 bg-white border border-gray-700 rounded-full"></div>
+        <div class="w-10 h-10 bg-blue-900 rounded-full"></div>
+    </aside> 
 </header>
 
 @yield("contenu")
