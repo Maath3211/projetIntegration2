5ddf517af90c7b92c75ead4754d425a3fb7865a2
Loick Michaud <139803354+naijel2103@users.noreply.github.com>
2025-01-29 13:56:56 -0500

    layout+css

--- projetIntegration2/resources/views/Layouts/app.blade.php
+++ projetIntegration2/resources/views/Layouts/app.blade.php
@@ -9,6 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" style="text/css" href="\css\GabaritCss.css">
         @yield("style")
    
 <meta charset="UTF-8">
@@ -16,10 +17,15 @@
 
 <body>
 <!-- Navigation principale -->
+<header>
+
+</header>
 
 @yield("contenu")
 
 
 <!-- Mettre le footer -->
+ <footer>
+</footer>
 </body>
 </html>
