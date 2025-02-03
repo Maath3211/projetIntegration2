37e36757ae8d06bc7aa929b2f0bd237e27bf7d9d
Thomas St-Amand <thomasstamand1@gmail.com>
2025-02-03 13:46:02 -0500

    Barre de nav

--- projetIntegration2/resources/views/Clans/accueilClans.blade.php
+++ projetIntegration2/resources/views/Clans/accueilClans.blade.php
@@ -1,27 +1,42 @@
 @extends('Layouts.app')
 @section('style')
     <link type="stylesheet" href="{{ asset('css/Clans.css') }}">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <style>
+        .conteneurImage{
+            background-image: url('{{ asset('img/workoutMasterLogo.jpg') }}');
+            background-size: cover;
+            background-position: center center;
+            height: 150px;
+        }
+    </style>
 @endsection()
 
 @section('contenu')
 
-<div class="contenuPrincipal flex-1 p-4">
+<div class="contenuPrincipal flex-1">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-2">
-                <div class="p-4">
-                    <h3>Column 1</h3>
-                    <p>Content for column 1 goes here.</p>
+            <div class="col-md-2 colonneChannels">
+                <div class="container">
+                    <div class="column">
+                        <div class="conteneurImage">
+                            <div class="texteSurImage">Workout Master</div>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-md-7">
-                <div class="p-4">
+                <div>
                     <h3>Column 2</h3>
                     <p>Content for column 2 goes here.</p>
                 </div>
             </div>
             <div class="col-md-3">
-                <div class="p-4">
+                <div>
                     <h3>Column 3</h3>
                     <p>Content for column 3 goes here.</p>
                 </div>
