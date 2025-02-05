ca40bf1a47cc8428fcc871211859c6e4e26dc923
Thomas St-Amand <thomasstamand1@gmail.com>
2025-02-05 12:33:43 -0500

    ajout des commentaires à l'interface de clans

--- projetIntegration2/resources/views/Clans/accueilClans.blade.php
+++ projetIntegration2/resources/views/Clans/accueilClans.blade.php
@@ -630,17 +630,25 @@
 </div>
 
 <script>
-    document.addEventListener("DOMContentLoaded", function() {
-        const canals = document.querySelectorAll('.canal');
 
+    // JavaScript qui soit s'exécuter une fois la page chargée
+    document.addEventListener("DOMContentLoaded", function() {
+
+        const canals = document.querySelectorAll('.canal');
+        const divScrollable = document.querySelector(".contenuScrollable");
+
+        // Pour montrer quel canal est actif actuellement
         canals.forEach(canal => {
             canal.addEventListener('click', function() {
                 canals.forEach(c => c.classList.remove('active'));
                 canal.classList.add('active');
+
+                // Quand on change de canal de chat, on scroll jusqu'en bas automatiquement
+                divScrollable.scrollTop = divScrollable.scrollHeight;
             });
         });
 
-        const divScrollable = document.querySelector(".contenuScrollable");
+        // Déroulement automatique jusqu'en bas des messages
         divScrollable.scrollTop = divScrollable.scrollHeight;
     });
 
