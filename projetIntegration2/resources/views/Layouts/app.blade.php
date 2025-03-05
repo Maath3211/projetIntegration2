0eaaed455221d0a064cb3a99012cf0cf7b06ca5f
Mathys Lessard <mathys.l.lessard@gmail.com>
2025-03-05 13:10:07 -0500

    drawer layout

--- projetIntegration2/resources/views/Layouts/app.blade.php
+++ projetIntegration2/resources/views/Layouts/app.blade.php
@@ -30,7 +30,53 @@
 
 <body class=" flex h-screen">
   <!-- Barre de navigation entre les clans et les messages privés entre autres -->
-  <header>
+  <button id="hamburgerBtn" class="hamburger-btn">
+    <span class="hamburger-line"></span>
+    <span class="hamburger-line"></span>
+    <span class="hamburger-line"></span>
+</button>
+
+<div id="mobileMenu" class="mobile-menu">
+    <!-- Navigation aux messages privés entre utilisateurs -->
+    <a href="#">
+        <div class="w-16 h-16 rounded-full overflow-hidden bullePersonnalisee">
+            <i class="fa-solid fa-comment fa-2xl"></i>
+        </div>
+    </a>
+
+    <!-- Navigation aux classements -->
+    <a href="#">
+        <div class="w-16 h-16 rounded-full overflow-hidden bullePersonnalisee">
+            <i class="fa-solid fa-ranking-star fa-2xl"></i>
+        </div>
+    </a>
+
+    <!-- Tous les clans dont l'utilisateur actuel fait partie -->
+    @if(isset($clans))
+        @foreach($clans as $clan)
+        <a href="{{ route('clan.montrer', ['id' => $clan->id]) }}">
+            <div class="w-16 h-16 rounded-full overflow-hidden">
+                <img src="{{ asset($clan->image) }}" class="object-cover w-full h-full">
+            </div>
+        </a>
+        @endforeach
+    @endif
+
+    <a id="creerClanMobile"> 
+        <div class="w-16 h-16 rounded-full overflow-hidden bullePersonnalisee creerClan">
+            <i class="fa-regular fa-square-plus fa-2xl"></i>
+        </div>
+    </a>
+
+    <form action="{{route('profil.deconnexion')}}" method="post" style="margin-top: auto; margin-bottom: 20px;">
+        @csrf
+        <button class="w-16 h-16 overflow-hidden">
+            <img src="{{ asset('img/logout.png') }}" class="object-cover w-full h-full">
+        </button>
+    </form>
+</div>
+
+  <header id="navBar">
     <aside class="w-20 text-white h-screen flex flex-col items-center py-4 space-y-4 p-5">
 
       <!-- Navigation aux messages privés entre utilisateurs -->
@@ -142,5 +188,6 @@
 <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 @yield('scripts')
 <script src="{{asset('js/gabaritJs.js')}}" crossorigin="anonymous"></script>
+<script src="{{asset('js/drawerGabarit.js')}}" crossorigin="anonymous"></script>
 
 </html>
