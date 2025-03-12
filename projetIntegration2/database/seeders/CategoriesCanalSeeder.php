68f55633573b81bd7d3d28b7c4fdd6a13716bed0
Thomas St-Amand <thomasstamand1@gmail.com>
2025-03-12 14:05:48 -0400

    Ajout de 4 clans et de leurs infos

--- projetIntegration2/database/seeders/CategoriesCanalSeeder.php
+++ projetIntegration2/database/seeders/CategoriesCanalSeeder.php
@@ -23,6 +23,54 @@ class CategoriesCanalSeeder extends Seeder
                 'clanId' => 1,
                 'categorie' => 'Compétition'
             ],
+            [
+                'clanId' => 2,
+                'categorie' => 'Général'
+            ],
+            [
+                'clanId' => 2,
+                'categorie' => 'Aide'
+            ],
+            [
+                'clanId' => 2,
+                'categorie' => 'Compétition'
+            ],
+            [
+                'clanId' => 3,
+                'categorie' => 'Général'
+            ],
+            [
+                'clanId' => 3,
+                'categorie' => 'Aide'
+            ],
+            [
+                'clanId' => 3,
+                'categorie' => 'Compétition'
+            ],
+            [
+                'clanId' => 4,
+                'categorie' => 'Général'
+            ],
+            [
+                'clanId' => 4,
+                'categorie' => 'Aide'
+            ],
+            [
+                'clanId' => 4,
+                'categorie' => 'Compétition'
+            ],
+            [
+                'clanId' => 5,
+                'categorie' => 'Général'
+            ],
+            [
+                'clanId' => 5,
+                'categorie' => 'Aide'
+            ],
+            [
+                'clanId' => 5,
+                'categorie' => 'Compétition'
+            ]
         ]);
     }
 }
