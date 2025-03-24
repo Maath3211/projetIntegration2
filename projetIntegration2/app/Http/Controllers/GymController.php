2ffd456aa75b03bb033493e49249b1e49800af68
Nicxx11 <90414449+Nicxx11@users.noreply.github.com>
2025-03-24 13:23:34 -0400

    last update

--- projetIntegration2/app/Http/Controllers/GymController.php
+++ projetIntegration2/app/Http/Controllers/GymController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GymController extends Controller
 {
 
     public function index()
     {
-        return view('localisation.localisation');
+        $utilisateur = Auth::user();
+        $clans = $utilisateur->clans;
+
+        return view('localisation.localisation', compact('clans'));
     }
 
 }
